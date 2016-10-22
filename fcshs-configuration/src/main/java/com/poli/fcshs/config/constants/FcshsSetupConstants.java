24bbb8c10ab72d30112df2cc4ce945bfa8d14e63
filipe <filipelopes15@gmail.com>
2016-10-22 19:50:28 -0300

    Adiciona classes de constantes do sistema e classe para leitura do arquivo de properties

---
+++ fcshs-configuration/src/main/java/com/poli/fcshs/config/constants/FcshsSetupConstants.java
@@ -0,0 +1,15 @@
+package com.poli.fcshs.config.constants;
+
+/**
+ * 
+ * @author Filipe Lopes create on 22/10/2016
+ *
+ */
+
+public class FcshsSetupConstants
+{
+
+	// Data template
+	public static final String DATA_TEMPLATE_DIRECTORY = "store.templates.xls";
+
+}
---
+++ fcshs-configuration/src/main/java/com/poli/fcshs/config/loader/FcshsPropertiesLoader.java
@@ -0,0 +1,55 @@
+package com.poli.fcshs.config.loader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * 
+ * @author Filipe Lopes create on 22/10/2016
+ * 
+ */
+
+public class FcshsPropertiesLoader
+{
+
+	private Properties fcshsProperties;
+	private static FcshsPropertiesLoader instance;
+
+	private FcshsPropertiesLoader()
+	{
+		this.loadFcshsProperties();
+	}
+
+	public static FcshsPropertiesLoader getInstance()
+	{
+		if (instance == null)
+		{
+			instance = new FcshsPropertiesLoader();
+		}
+		return instance;
+	}
+
+	public Properties loadFcshsProperties()
+	{
+		try
+		{
+			InputStream globalPropertiesStream = this.getClass().getClassLoader()
+					.getResourceAsStream("fcshs.properties");
+			fcshsProperties = new Properties();
+			fcshsProperties.load(globalPropertiesStream);
+
+			return fcshsProperties;
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException("Failed to load fcshs.properties file", e);
+		}
+	}
+
+	public String getPropertyByName(String name)
+	{
+		return this.fcshsProperties.getProperty(name);
+	}
+
+}
