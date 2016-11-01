74db1b07824e9ec0c872e96f68636a7ee8163dd4
filipe <filipelopes15@gmail.com>
2016-11-01 18:04:53 -0300

    Adiciona classe utils para metodos auxiliares no processo de gerar base de conhecimento

---
+++ fcshs-knowledge-base/src/main/java/com/poli/fcshs/generator/util/DataBaseGeneratorUtils.java
@@ -0,0 +1,16 @@
+package com.poli.fcshs.generator.util;
+
+/**
+ * @author Filipe Lopes created on 01/11/2016
+ * 
+ */
+
+public class DataBaseGeneratorUtils
+{
+
+	/*
+	 * Essa classe deve conter todos os metodos uteis para auxiliar na classe
+	 * principal desse pacote todos os metodos devem ser estaticos
+	 */
+
+}
