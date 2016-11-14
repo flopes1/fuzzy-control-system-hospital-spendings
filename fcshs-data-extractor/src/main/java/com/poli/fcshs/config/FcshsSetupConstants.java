e8fd6cf0a13897b14108d17eb3c841c08675007d
filipe <filipelopes15@gmail.com>
2016-11-14 20:01:51 -0300

    Move constantes de novas propertys para local correto

--- fcshs-data-extractor/src/main/java/com/poli/fcshs/config/FcshsSetupConstants.java
+++ fcshs-data-extractor/src/main/java/com/poli/fcshs/config/FcshsSetupConstants.java
@@ -12,6 +12,13 @@ public class FcshsSetupConstants
 	// Data template
 	public static final String DATA_TEMPLATE_DIRECTORY_CSV = "store.templates.csv";
 	public static final String DATA_TEMPLATE_DIRECTORY_XLS = "store.templates.xls";
-	
+
+	// Variables linguistics terms
+	public static final String DATA_RULES = "data.rules";
+	public static final String INPUT_TERMS = "input.terms";
+	public static final String OUTPUT_TERMS = "output.terms";
+
+	// Configurable system values
+	public static final String ITEM_MAX_VALUE_MULTIPLIFIER = "system.item.max.multiplifier";
 
 }
