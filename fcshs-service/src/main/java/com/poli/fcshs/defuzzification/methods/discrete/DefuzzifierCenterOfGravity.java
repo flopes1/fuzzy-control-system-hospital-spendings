b9bb94ef99e9dbd6d39a4a7a672db4a8bc514b6e
filipe <filipelopes15@gmail.com>
2016-11-15 00:18:31 -0300

    Adiciona metodo de defuzzifiação center of gravity

---
+++ fcshs-service/src/main/java/com/poli/fcshs/defuzzification/methods/discrete/DefuzzifierCenterOfGravity.java
@@ -0,0 +1,55 @@
+package com.poli.fcshs.defuzzification.methods.discrete;
+
+import java.util.HashMap;
+
+import com.poli.fcshs.defuzzification.methods.DefuzzifierDiscreteSet;
+import com.poli.fcshs.model.FuzzySet;
+import com.poli.fcshs.model.LinguisticVariableItem;
+import com.poli.fcshs.model.SystemOutputItem;
+
+/**
+ * @author Filipe Lopes created on 13/11/2016
+ * 
+ */
+
+public class DefuzzifierCenterOfGravity extends DefuzzifierDiscreteSet
+{
+
+	public DefuzzifierCenterOfGravity(LinguisticVariableItem outputInferenceItem)
+	{
+		super("Center of Gravity - COG", outputInferenceItem);
+	}
+
+	public SystemOutputItem defuzzify()
+	{
+		SystemOutputItem outputItem = new SystemOutputItem();
+
+		double x, y, sum = 0, sumWight = 0;
+
+		for (FuzzySet fuzzySet : this.getOutputInferenceItem().getFuzzySetItens())
+		{
+			HashMap<Double, Double> set = fuzzySet.getFuzzySetItens();
+			for (Double key : set.keySet())
+			{
+				y = set.get(key);
+				x = key;
+				sumWight += x * y;
+				sum += y;
+			}
+		}
+
+		double value = sum != 0 ? (sumWight / sum) : 0;
+
+		outputItem.setOutputName(this.getOutputInferenceItem().getLinguisticVariableName());
+		outputItem.setOutputValue(value);
+		outputItem.setOutputType(this.getOutputInferenceItem().getDomainType());
+
+		return outputItem;
+	}
+
+	public String getInformation()
+	{
+		return super.getInformtion();
+	}
+
+}
