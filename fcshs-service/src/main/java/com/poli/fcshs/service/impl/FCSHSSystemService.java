fe28185a4054d6a776228af75d67d5f9b301d000
filipe <filipelopes15@gmail.com>
2016-11-17 22:58:12 -0300

    Implementa parcialmente classe de service principal do sistema

--- fcshs-service/src/main/java/com/poli/fcshs/service/impl/FCSHSSystemService.java
+++ fcshs-service/src/main/java/com/poli/fcshs/service/impl/FCSHSSystemService.java
@@ -1,6 +1,13 @@
 package com.poli.fcshs.service.impl;
 
-import com.poli.fcshs.generator.KnowledgeBaseGenerator;
+import com.poli.fcshs.config.FcshsConstants;
+import com.poli.fcshs.config.FcshsPropertiesLoader;
+import com.poli.fcshs.config.FcshsSetupConstants;
+import com.poli.fcshs.defuzzification.Defuzzifier;
+import com.poli.fcshs.defuzzification.methods.continuous.DefuzzifierMeanMax;
+import com.poli.fcshs.defuzzification.methods.discrete.DefuzzifierCenterOfGravity;
+import com.poli.fcshs.model.LinguisticVariableItem;
+import com.poli.fcshs.model.SystemOutputItem;
 import com.poli.fcshs.service.IFCSHSSystemService;
 
 /**
@@ -11,35 +18,64 @@ import com.poli.fcshs.service.IFCSHSSystemService;
 public class FCSHSSystemService implements IFCSHSSystemService
 {
 
-	private KnowledgeBaseGenerator systemKnowledgeBase;
+	/*
+	 * System functional variables
+	 */
+
+	// fuzzi attribute
+	// inference attribute
+	private Defuzzifier defuzziMethod;
+
+	/*
+	 * System configurable variables
+	 */
+	private static final String DEFUZZI_METHOD = FcshsPropertiesLoader.getInstance()
+			.getPropertyByName(FcshsSetupConstants.DEFUZZIFICATION_METHOD);
 
 	public FCSHSSystemService()
 	{
-		this.systemKnowledgeBase = new KnowledgeBaseGenerator();
+	}
+
+	public String getSystemAnalysisByHospital(String hospitalName, String year)
+	{
+		// chama a fuzzificação para argumentos hospital e ano
+		// pega o retorno e passa para a inferencia que trata um hospital e um
+		// ano
+		// seta o retorno da inferencia na variavel abaixo
+		LinguisticVariableItem inferenceOutput = null;
+
+		this.initializeDefuzzification(inferenceOutput);
+		SystemOutputItem defuzziOutput = this.defuzziMethod.defuzzify();
+
+		return defuzziOutput.toString();
 	}
 
 	public String getSystemAnalysisByHospital(String hospitalName)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		// chama a fuzzificação para argumento só hospital (todos os anos i.e
+		// planilhas)
+		// pega o retorno e passa para a inferencia que trata um só hospital em
+		// todos os anos
+		// seta o retorno da inferencia na variavel abaixo
+		LinguisticVariableItem inferenceOutput = null;
+
+		this.initializeDefuzzification(inferenceOutput);
+		SystemOutputItem defuzziOutput = this.defuzziMethod.defuzzify();
+
+		return defuzziOutput.toString();
 	}
 
-	public String getSystemAnalysisByYear(String year)
+	private void initializeDefuzzification(LinguisticVariableItem outputInferenceItem)
 	{
-		// TODO Auto-generated method stub
-		return null;
-	}
+		if (DEFUZZI_METHOD.equals(FcshsConstants.DEFUZZI_METHOD_CONTINUOUS))
+		{
+			this.defuzziMethod = new DefuzzifierMeanMax(outputInferenceItem);
+		}
+		else
+		{
+			this.defuzziMethod = new DefuzzifierCenterOfGravity(outputInferenceItem);
+		}
 
-	public String getSystemAnalysisAllYears()
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getSystemAnalysisAllHospitalYears()
-	{
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
